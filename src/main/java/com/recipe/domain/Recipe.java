304681bc3b3cd9650ce028d7fff6b9078200c1e0
administrator <masoudider@yahoo.com>
2019-11-25 23:28:13 +0330

    initializing

---
+++ src/main/java/com/recipe/domain/Recipe.java
@@ -0,0 +1,15 @@
+package com.recipe.domain;
+
+public class Recipe {
+
+    private String description;
+    private Integer perTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    private Byte[] image;
+
+}
