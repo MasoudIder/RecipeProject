87ccb7e683fb62771de4463cdd4f6ac3510cfb15
administrator <masoudider@yahoo.com>
2019-12-19 01:04:41 +0330

    addData

--- src/main/java/com/recipe/repositories/CategoryRepository.java
+++ src/main/java/com/recipe/repositories/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.recipe.repositories;
 
 import com.recipe.domain.Category;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
